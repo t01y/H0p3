ad1222583e735df0f0e39c81ccc1462af4c98f59
but0n <but0n@icloud.com>
2016-12-10 15:36:56 +0800

    [MOD]: Deleted File 'do_flash.pl'

--- Software/src/main.c
+++ Software/src/main.c
@@ -1,7 +1,9 @@
 #include "stm32f10x.h"
 #include "FreeRTOS.h"
 #include "task.h"
+
 #include "bit.h"
+#include "uart.h"
 
 #define LED0 BIT_ADDR((GPIOA_BASE+12), 8)
 #define LED1 BIT_ADDR((GPIOD_BASE+12), 2)
@@ -35,17 +37,34 @@ void LedTask1() {
 		LED1 = 0;
 		vTaskDelay(100);
 		LED1 = 1;
-		vTaskDelay(600);
+		vTaskDelay(900);
+	}
+}
+void uart_task0() {
+	while(1) {
+		uart_sendStr("Task...[0]");
+		UART_CR();
+		vTaskDelay(250);
+	}
+}
+void uart_task1() {
+	while(1) {
+		uart_sendStr("[1]");
+		UART_CR();
+		vTaskDelay(100);
 	}
 }
 
 int main() {
 	// NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
-	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+	// RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
 	Init_led();
+	uart_init(72, 115200);
 
 	xTaskCreate(LedTask0, "LED_TASK0", 40, NULL, 1, NULL);
 	xTaskCreate(LedTask1, "LED_TASK1", 40, NULL, 2, NULL);
+	xTaskCreate(uart_task0, "UART_TASK0", 40, NULL, 3, NULL);
+	xTaskCreate(uart_task1, "UART_TASK1", 40, NULL, 4, NULL);
 	vTaskStartScheduler();
 	while(1);
 }
